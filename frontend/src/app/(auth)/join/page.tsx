3ec9403c56618243bb3a09357f4b568a047262e0
sohseyedi <sohseyedi@gmail.com>
2025-03-09 21:41:33 +0330

    feat(frontend): create authentication page

---
+++ frontend/src/app/(auth)/join/page.tsx
@@ -0,0 +1,9 @@
+import AuthContainer from "../_/components/AuthContainer";
+
+export default function AuthPage() {
+  return (
+    <section className="flex items-center justify-center h-screen">
+      <AuthContainer />
+    </section>
+  );
+}
