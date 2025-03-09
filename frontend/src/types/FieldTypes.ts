653f37343fd1f775c35e442e4ded14680d6b625c
sohseyedi <sohseyedi@gmail.com>
2025-03-09 21:30:58 +0330

    feat(frontend): add types for input fields

---
+++ frontend/src/types/FieldTypes.ts
@@ -0,0 +1,25 @@
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+export type ValidationSchemaTypes = {
+  required?: string;
+  minLength?: {
+    value: number;
+    message: string;
+  };
+  pattern?: {
+    value: RegExp;
+    message: string;
+  };
+};
+
+export type InputFieldTypes = {
+  label: string;
+  name: string;
+  type?: string;
+  placeholder?: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  validationSchema?: ValidationSchemaTypes;
+  value?: string;
+  onChange?: React.ChangeEventHandler;
+};
