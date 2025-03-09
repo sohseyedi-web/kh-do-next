87b4977c3e3ebe86b23091eb07d1d6d7719ecb9f
sohseyedi <sohseyedi@gmail.com>
2025-03-09 21:42:17 +0330

    feat(frontend): add HeaderLayout component with toggle switch and user authentication button

---
+++ frontend/src/app/(main)/_/components/HeaderLayout.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useState } from "react";
+import ToggleSwitch from "@/ui/ToggleSwitch";
+import UserAuthBtn from "./UserAuthBtn";
+
+const HeaderLayout = () => {
+  return (
+    <header className="py-3 md:px-7 px-5 flex items-center justify-between border-b border-[#e6e7ee]">
+      <div className="flex items-center gap-x-3">
+        <h2 className="text-3xl font-bold text-[#292a35]">دوربین</h2>
+      </div>
+      <div className="flex items-center gap-x-2">
+        <ToggleSwitch />
+        <UserAuthBtn />
+      </div>
+    </header>
+  );
+};
+
+export default HeaderLayout;
