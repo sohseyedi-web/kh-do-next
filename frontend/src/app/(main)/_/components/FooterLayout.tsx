7f67b5252453fa74cb1c964f2533445ea9c69d12
sohseyedi <sohseyedi@gmail.com>
2025-03-10 11:50:56 +0330

    style(frontend):responsive text alignment

--- frontend/src/app/(main)/_/components/FooterLayout.tsx
+++ frontend/src/app/(main)/_/components/FooterLayout.tsx
@@ -5,18 +5,22 @@ const Footer = () => {
   return (
     <footer className="bg-zinc-800 text-zinc-100 p-6 md:p-8 mt-20">
       <div className="container mx-auto grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-8 text-center md:text-right">
-        <div>
-          <h2 className="text-5xl font-bold text-teal-500">دوربین</h2>
-          <p className="text-lg mt-2">مرجع تخصصی دوربین حفاظتی</p>
-          <div className="flex justify-center md:justify-start gap-3 mt-4 text-xl">
+        <div className="md:text-center text-right">
+          <h2 className="md:text-5xl text-3xl font-bold text-teal-500">
+            دوربین
+          </h2>
+          <p className="md:text-lg mt-2">مرجع تخصصی دوربین حفاظتی</p>
+          <div className="flex justify-start gap-3 mt-4 md:text-xl">
             <FaInstagram size={20} className="text-teal-500" />
             <FaTelegram size={20} className="text-teal-500" />
             <FaWhatsapp size={20} className="text-teal-500" />
           </div>
         </div>
 
-        <div>
-          <h3 className="text-2xl font-semibold text-teal-500">دوربین</h3>
+        <div className="md:text-center text-right">
+          <h3 className="md:text-2xl text-xl font-semibold text-teal-500">
+            دوربین
+          </h3>
           <ul className="mt-4 space-y-4">
             <li>بلاگ دوربین</li>
             <li>تماس با ما</li>
@@ -24,8 +28,10 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div>
-          <h3 className="text-2xl font-semibold text-teal-500">فروشگاه</h3>
+        <div className="md:text-center text-right">
+          <h3 className="md:text-2xl text-xl font-semibold text-teal-500">
+            فروشگاه
+          </h3>
           <ul className="mt-4 space-y-4">
             <li>محصولات ما</li>
             <li>پشتیبانی</li>
@@ -33,8 +39,10 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div>
-          <h3 className="text-2xl font-semibold text-teal-500">مجوز ها</h3>
+        <div className="md:text-center text-right">
+          <h3 className="md:text-2xl text-xl font-semibold text-teal-500">
+            مجوز ها
+          </h3>
           <ul className="mt-4 flex items-center gap-x-3">
             <div className="bg-white p-4 rounded-xl">
               <Image
@@ -58,23 +66,6 @@ const Footer = () => {
           <p className="mt-2">آدرس : اهواز نادری خیابون اول</p>
         </div>
       </div>
-
-      {/* <div className="container mx-auto flex flex-col md:flex-row justify-center md:justify-start items-center gap-4 mt-6">
-        <div className="bg-white p-4 rounded-lg shadow-md w-32 md:w-40 h-40 md:h-56 flex justify-center items-center">
-          <img
-            src="https://via.placeholder.com/100"
-            alt="Logo"
-            className="w-16 md:w-20"
-          />
-        </div>
-        <div className="bg-white p-4 rounded-lg shadow-md w-32 md:w-40 h-40 md:h-56 flex justify-center items-center">
-          <img
-            src="https://via.placeholder.com/100"
-            alt="Placeholder"
-            className="w-16 md:w-20"
-          />
-        </div>
-      </div> */}
     </footer>
   );
 };
