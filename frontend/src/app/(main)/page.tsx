12df98f1da5b14dd93f3c990df7512551495f16a
sohseyedi <sohseyedi@gmail.com>
2025-03-10 10:51:00 +0330

    chore(frontend): update HomePage styles with teal color scheme for header and button

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -8,7 +8,7 @@ function HomePage() {
     <>
       <HeaderLayout />
       <section className="pt-16 max-w-4xl text-center mx-auto container space-y-6">
-        <h1 className="text-5xl font-extrabold">دوربین</h1>
+        <h1 className="text-5xl font-extrabold text-teal-500">دوربین</h1>
         <h5 className="md:text-3xl text-xl font-bold">
           بخونید، یاد بگیرید و آنلاین خرید کنید
         </h5>
@@ -17,13 +17,13 @@ function HomePage() {
         </p>
         <Link
           href={"/blogs"}
-          className="md:w-[280px] w-[200px] bg-zinc-800 text-white mx-auto h-[45px] mt-8 text-lg rounded-xl flex items-center justify-center hover:bg-zinc-900 transition-all duration-300 font-medium"
+          className="md:w-[280px] w-[200px] bg-teal-500 text-white mx-auto h-[45px] mt-8 text-lg rounded-xl flex items-center justify-center hover:bg-teal-600 transition-all duration-300 font-medium"
         >
           شروع کنید
           <RiArrowLeftSLine size={28} />
         </Link>
       </section>
-      <Stats/>
+      <Stats />
     </>
   );
 }
