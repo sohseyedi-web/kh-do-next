bab3b67ae4d0e1cbd21082b98366eea56f6bb6f8
sohseyedi <sohseyedi@gmail.com>
2025-03-10 10:52:02 +0330

    style(frontend): update Stat component styles with shadow and teal text color

--- frontend/src/ui/Stat.tsx
+++ frontend/src/ui/Stat.tsx
@@ -8,11 +8,11 @@ type StatBoxType = {
 
 const Stat = ({ label, value }: StatBoxType) => {
   return (
-    <div className="flex items-center flex-col justify-center rounded-xl border-2 w-full h-[190px]">
-      <h3 className="text-3xl font-bold">
+    <div className="flex items-center flex-col justify-center rounded-xl border shadow w-full h-[190px]">
+      <h3 className="text-3xl font-bold text-teal-500">
         <CountUp start={0} end={value} duration={2.5} />
       </h3>
-      <p className="text-gray-800 text-lg font-semibold mt-2">{label}</p>
+      <p className=" text-lg font-semibold mt-2">{label}</p>
     </div>
   );
 };
