df289e3d6c4238045f219dfe4da2a74f5b18dce9
sohseyedi <sohseyedi@gmail.com>
2025-03-10 10:51:09 +0330

    chore(frontend): update HeaderLayout title color to teal

--- frontend/src/app/(main)/_/components/HeaderLayout.tsx
+++ frontend/src/app/(main)/_/components/HeaderLayout.tsx
@@ -7,7 +7,7 @@ const HeaderLayout = () => {
   return (
     <header className="py-3 md:px-7 px-5 flex items-center justify-between border-b border-[#e6e7ee]">
       <div className="flex items-center gap-x-3">
-        <h2 className="text-3xl font-bold text-[#292a35]">دوربین</h2>
+        <h2 className="text-3xl font-bold text-teal-500">دوربین</h2>
       </div>
       <div className="flex items-center gap-x-2">
         <ToggleSwitch />
