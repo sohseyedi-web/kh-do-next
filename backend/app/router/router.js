5aef26943a5c67e353c2af990eb256af9f6cc368
sohseyedi <sohseyedi@gmail.com>
2025-03-10 17:30:28 +0330

    chore(backend):add new routes in project

--- backend/app/router/router.js
+++ backend/app/router/router.js
@@ -1,7 +1,11 @@
 const { verifyAccessToken } = require("../http/middlewares/auth.middleware");
 const { userAuthRoutes } = require("./auth");
+const { categoryRoutes } = require("./category");
+const { postRoutes } = require("./post");
 const router = require("express").Router();
 
 router.use("/user", userAuthRoutes);
+router.use("/post", postRoutes);
+router.use("/category", categoryRoutes);
 
 module.exports = router;
