ab79df139d1961877499e8ac0a3937fe2d82c281
sohseyedi <sohseyedi@gmail.com>
2025-03-10 10:31:17 +0330

    feat(frontend): implement authentication middleware for user profile fetching

---
+++ frontend/src/utils/middlewareAuth.ts
@@ -0,0 +1,18 @@
+import type { NextRequest } from "next/server";
+import { toStringCookies } from "./toStringCookies";
+
+export default async function middlewareAuth(req: NextRequest) {
+  const { data } = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/user/profile`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Cookie: toStringCookies(req.cookies),
+      },
+    }
+  ).then((res) => res.json());
+
+  const { user } = data || {};
+  return { user };
+}
