5d9014c82cb95534056704bd443a9458c7743b55
sohseyedi <sohseyedi@gmail.com>
2025-03-11 11:26:30 +0330

    feat(frontend): enhance PanelHome component with StatsWrapper and LatesBlog sections

--- frontend/src/app/(panel)/panel/page.tsx
+++ frontend/src/app/(panel)/panel/page.tsx
@@ -1,5 +1,17 @@
-export default async function PanelHome(){
-    return(
-        <div>sohiel</div>
-    )
+import { Suspense } from "react";
+import StatsWrapper from "../_/components/stats/StatsWrapper";
+import LatesBlog from "../_/components/blogs/LatesBlog";
+
+export default async function PanelHome() {
+  return (
+    <section className="max-w-7xl mx-auto">
+      <Suspense>
+        <StatsWrapper />
+      </Suspense>
+      <hr className="my-3 bg-zinc-700" />
+      <Suspense>
+        <LatesBlog />
+      </Suspense>
+    </section>
+  );
 }
