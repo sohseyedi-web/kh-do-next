02ba65bb5e1a2848673b09f18c4507c558a13612
sohseyedi <sohseyedi@gmail.com>
2025-03-13 19:00:14 +0330

    chore(frontend): add GET_ALL_USERS endpoint

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -2,5 +2,6 @@ export const REGISTER = `/user/signup`;
 export const LOGIN = `/user/signin`;
 export const USER_PROFILE = `/user/profile`;
 export const LOG_OUT = `/user/logout`;
+export const GET_ALL_USERS = `/user/list`;
 
 export const GET_CATEGORY = `/category/list`;
