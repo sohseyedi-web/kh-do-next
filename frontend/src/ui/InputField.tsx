0e32e9566942f176c6c05dbcde353e4e6542f89f
sohseyedi <sohseyedi@gmail.com>
2025-03-13 11:10:10 +0330

    chore(frontend): support textarea for long inputs

--- frontend/src/ui/InputField.tsx
+++ frontend/src/ui/InputField.tsx
@@ -1,5 +1,4 @@
 import { InputFieldTypes } from "@/types/FieldTypes";
-import { get } from "react-hook-form";
 
 function InputField({
   label,
@@ -11,26 +10,41 @@ function InputField({
   validationSchema,
   value,
   onChange,
+  long = false,
 }: InputFieldTypes) {
   return (
     <div className="w-full relative">
-      <label htmlFor={name} className="block mb-1 text-zinc-800 cursor-pointer">
+      <label
+        htmlFor={name}
+        className="block mb-1 text-zinc-800 cursor-pointer w-full"
+      >
         {label}
       </label>
-      <input
-        autoComplete="off"
-        className="placeholder:text-zinc-500 mb-2 text-zinc-800 w-full border focus:border-zinc-300 focus:shadow focus:bg-white md:h-[55px] h-[45px] px-3 transition-all duration-300 rounded-2xl outline-none"
-        type={type}
-        id={name}
-        value={value}
-        defaultValue={value}
-        placeholder={placeholder}
-        {...register(name, validationSchema)}
-        onChange={onChange}
-      />
-      {get(errors, `${name as string}.message`, null) && (
+      {long ? (
+        <textarea
+          autoComplete="off"
+          className="placeholder:text-zinc-500 mb-2 p-3 text-zinc-800 w-full border focus:border-zinc-300 focus:shadow focus:bg-white h-[125px] resize-none transition-all duration-300 rounded-2xl outline-none"
+          id={name}
+          value={value}
+          placeholder={placeholder}
+          {...register(name, validationSchema)}
+          onChange={onChange}
+        />
+      ) : (
+        <input
+          autoComplete="off"
+          className="placeholder:text-zinc-500 mb-2 text-zinc-800 w-full border focus:border-zinc-300 focus:shadow focus:bg-white md:h-[55px] h-[45px] px-3 transition-all duration-300 rounded-2xl outline-none"
+          type={type}
+          id={name}
+          value={value}
+          placeholder={placeholder}
+          {...register(name, validationSchema)}
+          onChange={onChange}
+        />
+      )}
+      {errors[name]?.message && (
         <span className="text-red-500 font-semibold">
-          {get(errors, `${name as string}.message`, null)}
+          {errors[name]?.message as string}
         </span>
       )}
     </div>
