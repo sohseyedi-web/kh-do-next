e52ced47d3f2022166392888c7e6a9d1d41437e8
sohseyedi <sohseyedi@gmail.com>
2025-03-13 11:10:30 +0330

    feat(frontend): add SelectField component for enhanced form selection

---
+++ frontend/src/ui/SelectField.tsx
@@ -0,0 +1,41 @@
+import { SelectFieldTypes } from "@/types";
+import { get } from "react-hook-form";
+
+function SelectField({
+  label,
+  name,
+  register,
+  options,
+  errors,
+  validationSchema,
+}: SelectFieldTypes) {
+  return (
+    <div className="relative">
+      <label htmlFor={name} className="block mb-1 text-zinc-800 cursor-pointer">
+        {label}
+      </label>
+      <select
+        className="mb-2 text-zinc-800 w-full cursor-pointer border focus:border-zinc-300 focus:shadow focus:bg-white md:h-[55px] h-[45px] px-3 transition-all duration-300 rounded-2xl outline-none"
+        {...register(name, validationSchema)}
+        id={name}
+        name={name}
+      >
+        {options.map((option) => (
+          <option
+            className="bg-red-300"
+            key={option.value}
+            value={option.value}
+          >
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {get(errors, `${name as string}.message`, null) && (
+        <span className="text-red-500 font-semibold">
+          {get(errors, `${name as string}.message`, null)}
+        </span>
+      )}
+    </div>
+  );
+}
+export default SelectField;
