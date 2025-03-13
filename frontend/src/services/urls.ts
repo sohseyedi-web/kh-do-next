af95954812ae66b6cf50539fee17bc5a5b46d279
sohseyedi <sohseyedi@gmail.com>
2025-03-13 11:38:25 +0330

    chore(frontend): add GET_CATEGORY URL

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -2,3 +2,5 @@ export const REGISTER = `/user/signup`;
 export const LOGIN = `/user/signin`;
 export const USER_PROFILE = `/user/profile`;
 export const LOG_OUT = `/user/logout`;
+
+export const GET_CATEGORY = `/category/list`;
