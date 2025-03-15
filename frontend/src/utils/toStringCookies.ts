6f1fb08db156ef4ca3df8d583046c40132c235f5
sohseyedi <sohseyedi@gmail.com>
2025-03-15 18:55:13 +0330

    chore(utils): update toStringCookies to handle async cookie retrieval

--- frontend/src/utils/toStringCookies.ts
+++ frontend/src/utils/toStringCookies.ts
@@ -1,11 +1,18 @@
+import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
+
 interface Cookies {
-  getAll: () => { name: string; value: string }[];
+  getAll: () => Promise<{ name: string; value: string }[]>;
 }
 
-export const toStringCookies = (cookies: Cookies): string => {
+export const toStringCookies = async (
+  cookies: RequestCookies
+): Promise<string> => {
   let strCookie = "";
-  cookies
-    .getAll()
-    .forEach((item) => (strCookie += `${item?.name}=${item?.value}; `));
+  const allCookies = await cookies.getAll();
+
+  allCookies.forEach((item) => {
+    strCookie += `${item.name}=${item.value}; `;
+  });
+
   return strCookie;
 };
