f8ca3bc5939184afa12f32290dffd1c8ad3bf915
sohseyedi <sohseyedi@gmail.com>
2025-03-15 18:56:50 +0330

    chore(frontend): update StatsWrapper to fetch and display dynamic category and post counts

--- frontend/src/app/(panel)/_/components/stats/StatsWrapper.tsx
+++ frontend/src/app/(panel)/_/components/stats/StatsWrapper.tsx
@@ -1,15 +1,28 @@
 import { HiOutlineUserGroup } from "react-icons/hi2";
 import { IoDocumentTextOutline, IoChatbubblesOutline } from "react-icons/io5";
 import Card from "./Card";
+import { fetchCardData } from "@/services/data";
 
-const StatsWrapper = () => {
+export default async function StatsWrapper() {
+  const { numOfCategories, numOfPosts } = await fetchCardData();
+
   return (
     <div className="grid md:grid-cols-1 lg:grid-cols-3 grid-cols-3 gap-6">
-      <Card title="کاربران" value={10} icon={<HiOutlineUserGroup className="lg:size-10 size-7"/>} />
-      <Card title="بلاگ ها" value={10} icon={<IoDocumentTextOutline className="lg:size-10 size-7"/>} />
-      <Card title="نظرات" value={10} icon={<IoChatbubblesOutline className="lg:size-10 size-7"/>} />
+      <Card
+        title="کاربران"
+        value={10}
+        icon={<HiOutlineUserGroup className="lg:size-10 size-7" />}
+      />
+      <Card
+        title="بلاگ ها"
+        value={numOfPosts}
+        icon={<IoDocumentTextOutline className="lg:size-10 size-7" />}
+      />
+      <Card
+        title="دسته بندی ها"
+        value={numOfCategories}
+        icon={<IoChatbubblesOutline className="lg:size-10 size-7" />}
+      />
     </div>
   );
-};
-
-export default StatsWrapper;
+}
