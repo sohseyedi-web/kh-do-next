ade2c50e8f6dfc556db571ec2a63806f1d1830c9
sohseyedi <sohseyedi@gmail.com>
2025-03-16 11:51:46 +0330

    feat(frontend): add BlogHeader component for blog listing page

---
+++ frontend/src/app/(panel)/panel/blogs/_/components/BlogHeader.tsx
@@ -0,0 +1,24 @@
+"use client";
+import {
+  PiCaretUpDownBold,
+  PiListBulletsBold,
+  PiGridFourBold,
+} from "react-icons/pi";
+
+const BlogHeader = () => {
+  return (
+    <header className="flex items-center justify-between">
+      <h3 className="text-2xl font-semibold text-teal-500">لیست بلاگ ها</h3>
+      <div className="flex items-center gap-x-3">
+        <button className="flex items-center justify-center size-11 rounded-2xl bg-teal-500 text-white">
+          <PiListBulletsBold size={24} className="rotate-180" />
+        </button>
+        <button className="flex items-center justify-center size-11 rounded-2xl bg-teal-500 text-white">
+          <PiCaretUpDownBold size={24} />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default BlogHeader;
