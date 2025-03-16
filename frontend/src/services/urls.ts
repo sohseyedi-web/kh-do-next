ebcdf73582bfd7ce8cc00e3de2cbd9c83a4c3eed
sohseyedi <sohseyedi@gmail.com>
2025-03-16 11:52:53 +0330

    chore(frontend): add UPDATE_USER endpoint

--- frontend/src/services/urls.ts
+++ frontend/src/services/urls.ts
@@ -3,6 +3,7 @@ export const LOGIN = `/user/signin`;
 export const USER_PROFILE = `/user/profile`;
 export const LOG_OUT = `/user/logout`;
 export const GET_ALL_USERS = `/user/list`;
+export const UPDATE_USER = `/user/update`;
 
 export const GET_CATEGORY = `/category/list`;
 export const CREATE_CATEGORY = `/category/add`;
