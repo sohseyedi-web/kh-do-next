12a70dc798454672067572a898b9be07ca75060c
sohseyedi <sohseyedi@gmail.com>
2025-03-16 11:51:40 +0330

    feat(frontend): create BlogPage component with header and blog lists

---
+++ frontend/src/app/(panel)/panel/blogs/page.tsx
@@ -0,0 +1,22 @@
+import queryString from "query-string";
+import BlogHeader from "./_/components/BlogHeader";
+import BlogLists from "./_/components/BlogLists";
+import { Suspense } from "react";
+
+interface BlogPageProps {
+  searchParams: Record<string, string>;
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const query = queryString.stringify(searchParams);
+
+  return (
+    <>
+      <BlogHeader />
+      <hr className="bg-zinc-700 my-3" />
+      <Suspense key={query}>
+        <BlogLists query={query} />
+      </Suspense>
+    </>
+  );
+}
