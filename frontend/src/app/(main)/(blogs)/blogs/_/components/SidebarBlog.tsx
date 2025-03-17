26e97580c11f52f3610eb6ab5fd687c5967facc6
sohseyedi <sohseyedi@gmail.com>
2025-03-17 21:30:46 +0330

    chore(frontend): refactor SidebarBlog component to use SidebarLayout and simplify structure

--- frontend/src/app/(main)/(blogs)/blogs/_/components/SidebarBlog.tsx
+++ frontend/src/app/(main)/(blogs)/blogs/_/components/SidebarBlog.tsx
@@ -1,26 +1,16 @@
 "use client";
-import { useResponsive } from "@/context/ResponsiveProvider";
-import Back from "@/ui/Back";
+import SidebarLayout from "@/components/SidebarWrapper";
 
 const SidebarBlog = () => {
-  const { active } = useResponsive();
-
   return (
-    <>
-      <Back />
-      <aside
-        className={`${
-          active ? "w-[18%] right-0 top-0" : "-right-52 w-0 top-0"
-        } fixed z-50 lg:relative h-screen p-4 transition-all duration-300`}
-      >
-        <div className="rounded-xl border border-zinc-200 p-4 h-full">
-          <h4 className="text-2xl font-bold text-teal-500">دوربین</h4>
-          <div className="pt-7">
-            <ul className="flex flex-col gap-y-3"></ul>
-          </div>
+    <SidebarLayout className="h-screen p-4 lg:bg-transparent bg-white">
+      <div className="rounded-xl lg:border lg:border-zinc-200 lg:p-4 p-2 h-full ">
+        <h4 className="text-2xl font-bold text-teal-500">دوربین</h4>
+        <div className="pt-7">
+          <ul className="flex flex-col gap-y-3">soheil</ul>
         </div>
-      </aside>
-    </>
+      </div>
+    </SidebarLayout>
   );
 };
 
