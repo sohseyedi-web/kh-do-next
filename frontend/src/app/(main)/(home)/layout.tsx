be85c152c879f64011012e09db105843d0b50bb3
sohseyedi <sohseyedi@gmail.com>
2025-03-17 17:40:25 +0330

    feat(frontend): create RootLayout component for main application layout

---
+++ frontend/src/app/(main)/(home)/layout.tsx
@@ -0,0 +1,31 @@
+import { Toaster } from "react-hot-toast";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import vazirFont from "@/constants/localFonts";
+import "@/styles/globals.css";
+import FooterLayout from "../_/components/FooterLayout";
+
+export const metadata = {
+  title: "دوربین / صفحه اصلی",
+  description: "دوربین فروشگاه آنلاین برای دوربین های حفاظتی",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}
+      >
+        <ReactQueryProvider>
+          <Toaster />
+          {children}
+          <FooterLayout />
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
