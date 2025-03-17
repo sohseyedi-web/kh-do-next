a6f4c60195a0f5a820330a414e45da1e0c511550
sohseyedi <sohseyedi@gmail.com>
2025-03-17 17:40:14 +0330

    feat(frontend): create layout component for individual blog posts

---
+++ frontend/src/app/(main)/(blogSlug)/[blogslug]/layout.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "react-hot-toast";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import vazirFont from "@/constants/localFonts";
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "دوربین / بلاگ",
+  description: "دوربین فروشگاه آنلاین برای دوربین های حفاظتی",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}
+      >
+        <ReactQueryProvider>
+          <Toaster />
+          {children}
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
