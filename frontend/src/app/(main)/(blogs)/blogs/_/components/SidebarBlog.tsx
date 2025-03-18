14246d4a4d9ef3a665d13bcc49892af82e3367af
sohseyedi <sohseyedi@gmail.com>
2025-03-18 12:05:05 +0330

    chore(frontend): add CustomNavlink

--- frontend/src/app/(main)/(blogs)/blogs/_/components/SidebarBlog.tsx
+++ frontend/src/app/(main)/(blogs)/blogs/_/components/SidebarBlog.tsx
@@ -1,5 +1,7 @@
 "use client";
 import SidebarLayout from "@/components/SidebarWrapper";
+import { CustomNavlink } from "@/ui/CustomNavlink";
+import { HiOutlineHome } from "react-icons/hi";
 
 const SidebarBlog = () => {
   return (
@@ -7,7 +9,12 @@ const SidebarBlog = () => {
       <div className="rounded-xl lg:border lg:border-zinc-200 lg:p-4 p-2 h-full ">
         <h4 className="text-2xl font-bold text-teal-500">دوربین</h4>
         <div className="pt-7">
-          <ul className="flex flex-col gap-y-3">soheil</ul>
+          <ul className="flex flex-col gap-y-3">
+            <CustomNavlink to={"/panel"}>
+              <HiOutlineHome size={26} />
+              <h6>صفحه اصلی</h6>
+            </CustomNavlink>
+          </ul>
         </div>
       </div>
     </SidebarLayout>
