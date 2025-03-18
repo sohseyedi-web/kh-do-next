bfed3422c90fd257d211f066e31e6b3bcbf4a7e8
sohseyedi <sohseyedi@gmail.com>
2025-03-18 12:04:12 +0330

    chore(frontend): update BlogPage to use await for cookies

--- frontend/src/app/(main)/(blogs)/blogs/page.tsx
+++ frontend/src/app/(main)/(blogs)/blogs/page.tsx
@@ -3,7 +3,7 @@ import { toStringCookies } from "@/utils/toStringCookies";
 import { cookies } from "next/headers";
 import queryString from "query-string";
 import BlogBox from "./_/components/BlogBox";
-import { ReadonlyURLSearchParams } from "next/navigation";
+import { BlogTypes } from "@/types";
 
 interface BlogPageProps {
   searchParams: Promise<any>;
@@ -12,14 +12,14 @@ interface BlogPageProps {
 export default async function BlogPage({ searchParams }: BlogPageProps) {
   const queries = queryString.stringify(searchParams);
   // set headers:
-  const cookieStore = cookies();
-  const options = toStringCookies(cookieStore as any);
+  const cookieStore = await cookies();
+  const options = toStringCookies(cookieStore);
 
-  const { posts, totalPages } = await getAllPostsApi(queries, options);
+  const { posts } = await getAllPostsApi(queries, options);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 cursor-pointer mt-5 gap-4">
-      {posts?.map((post: any) => (
+      {posts?.map((post: BlogTypes) => (
         <BlogBox key={post._id} blog={post} />
       ))}
     </div>
